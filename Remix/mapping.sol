affb24f84c583fa6897e7aded9d2c2d41bc0cd2a
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-02-06 17:10:37 +0700

    Update mapping.sol

--- Remix/mapping.sol
+++ Remix/mapping.sol
@@ -1,2 +1,10 @@
 SYNTAX:
   mapping(key_type => key_value) mappingName;
+
+- key_type thường là kiểu dữ liệu.
+- kiểu struct và các kiểu mapping khác không thể sử dụng làm key_type.
+- key_value có thể sử dụng cả struct và các kiểu mapping khác.
+
+THỰC TẾ:
+  mapping(address => uint256) balance;
+  
